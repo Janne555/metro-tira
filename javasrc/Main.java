25812f5fe21bd86acfd4d29765b2e7c1f71a5f3e
Janne Lavila <Janne555@users.noreply.github.com>
2020-04-04 17:30:38 +0300

    tehtävä 6 tehty

---
+++ javasrc/Main.java
@@ -0,0 +1,12 @@
+package javasrc;
+
+public class Main {
+  public static void main(String[] args) {
+    Stack stack = new Stack();
+    stack.push("ilma");
+    stack.push("vesi");
+    stack.push("tuli");
+    stack.push("maa");
+    stack.print();
+  }
+}
